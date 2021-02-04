721432eb0039cb2e2a41a4681711cdf9f2d03ca5
hansilyogi <hansilyogi@gmail.com>
2021-02-04 12:05:52 +0530

    4-Feb-genopt

--- userprofile.php
+++ userprofile.php
@@ -404,6 +404,12 @@ $data_2 =  $result_user['Data'];
                 </div>
             </div>
         </div>
+
+        <?php include "footer.php" ?>
+
+        <!-- Back to Top -->
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
         <script src="lib/easing/easing.min.js"></script>
