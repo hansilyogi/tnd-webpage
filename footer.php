adb9a368ea22b19fba55c243e8b4611b6c29048a
hansilyogi <hansilyogi@gmail.com>
2021-02-04 11:51:20 +0530

    4-Feb-genopt

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!-- Footer Start -->
+<div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-widget">
+                            <h3 class="title">Get in Touch</h3>
+                            <div class="contact-info">
+                                <table class="table borderless" style="color:white;">
+                                    <tbody>
+                                        <tr>
+                                            <td><i class="fa fa-home" style="color:red"></i></td>
+                                            <td>603, Trinity Orion, Beside Jolly Residency, Main Road, near Vijaya Laxmi Hall, Vesu, Surat, Gujarat 395007</td>
+                                        </tr>
+                                        <tr>
+                                            <td><i class="fa fa-envelope" style="color:blue"></i></td>
+                                            <td>info@thenationaldawn.in</td>
+                                        </tr>
+                                        <tr>
+                                            <td><i class="fa fa-phone" style="color:lightgreen"></i></td>
+                                            <td>+91 261 3501600, +919054151500</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-widget">
+                            <img src="img/footer-01.jpg" style="padding-left : 45px;">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-widget">
+                            <h3 class="title">Follow Us On Social Media</h3>
+                            <div class="contact-info">
+                                <div class="social">
+                                    <a href="https://www.facebook.com/thenationaldawn/"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://twitter.com/thenationaldawn"><i class="fab fa-twitter"></i></a>
+                                    <a href="https://www.instagram.com/thenationaldawn/"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://in.linkedin.com/in/the-national-dawn-b845421a1"><i class="fab fa-linkedin-in"></i></a>
+                                    <!-- <a href=""><i class="fab fa-youtube"></i></a>-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+        <!-- Footer Bottom Start -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 copyright">
+                        <p>Copyright &copy; <a href="https://htmlcodex.com">The National Dawn</a>. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer Bottom End -->
--- header.php
+++ header.php
@@ -1,55 +1,37 @@
-<!-- Top Bar Start -->
-<!--  <div class="top-bar">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="tb-contact">
-                    <p><i class="fas fa-envelope"></i>info@mail.com</p>
-                    <p><i class="fas fa-phone-alt"></i>+012 345 6789</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="tb-menu">
-                    <a href="">About</a>
-                    <a href="">Privacy</a>
-                    <a href="">Terms</a>
-                    <a href="">Contact</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-<!-- Top Bar Start -->
-<!-- Brand Start -->
 <div class="brand">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-3 col-md-4">
+            <div class="col-lg-4 col-md-4 col-sm-4">
                 <div class="b-logo">
-                    <a href="index.html">
+                    <a href="index.php">
                         <img src="img/logo.png" alt="Logo">
                     </a>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-4">
-                <div class="b-ads">
-                    <a href="https://htmlcodex.com">
-                        <!--<img src="img/ads-1.jpg" alt="Ads">-->
-                    </a>
+            <!-- <div class="col-lg-4 col-md-4">
+                <form class="form-inline" style="padding-top:15px;">
+                    <div class="form-group">
+                        <input class="form-control" type="search" placeholder="Search...." aria-label="Search" style="width:60% !important">
+                    </div>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> Search</button>
+                </form>
+            </div> -->
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="input-group rounded">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+                        aria-describedby="search-addon" />
+                    <span class="input-group-text border-0" id="search-addon">
+                        <i class="fas fa-search"></i>
+                    </span>
                 </div>
             </div>
-            
-            <div class="col-lg-3 col-md-4">
-                    <div class="social ml-auto">
+            <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="social ml-auto">
                     <a href="https://www.facebook.com/thenationaldawn/"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="https://twitter.com/thenationaldawn"><i class="fab fa-twitter"></i></a>
-                                    <a href="https://www.instagram.com/thenationaldawn/"><i class="fab fa-instagram"></i></a>
-                                    <a href="https://in.linkedin.com/in/the-national-dawn-b845421a1"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                <!--<div class="b-search">
-                    <input type="text" placeholder="Search">
-                    <button><i class="fa fa-search"></i></button>
-                </div>-->
+                    <a href="https://twitter.com/thenationaldawn"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.instagram.com/thenationaldawn/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://in.linkedin.com/in/the-national-dawn-b845421a1"><i class="fab fa-linkedin-in"></i></a>
+                </div>
             </div>
         </div>
     </div>
